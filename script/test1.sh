13c57d63381276f6bcc23bee8efca7feba8a08fb
Siryus18 <siryus@live.it>
2021-12-10 15:16:38 +0100

    Aggiunta cartella script.

---
+++ script/test1.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+
+./client -t 200 -f mysock -w test -W test/filepesante -D Wdir -r test/filepesante -R n=3 -d Rdir -l test/filepesante -u test/filepesante -c test/filepesante -p
+
+exit 0
