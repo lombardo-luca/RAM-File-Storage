31fcb24189a7d7d6f6f623983320fae88fce8cd3
Siryus18 <siryus@live.it>
2021-12-13 17:42:59 +0100

    Documentata api.h. Aggiunti file di test.

--- includes/api.h
+++ includes/api.h
@@ -5,12 +5,10 @@
 #define O_LOCK 2
 
 /**
- * Viene aperta una connessione AF_UNIX al socket file sockname. Se il server non accetta immediatamente la
- * richiesta di connessione, la connessione da parte del client viene ripetuta dopo ‘msec’ millisecondi e fino allo
- * scadere del tempo assoluto ‘abstime’ specificato come terzo argomento. Ritorna 0 in caso di successo, -1 in caso
- * di fallimento, errno viene settato opportunamente.
+ * Viene aperta una connessione AF_UNIX al socket file sockname. Se il server non accetta immediatamente la richiesta di connessione, 
+ * la connessione da parte del client viene ripetuta dopo ‘msec’ millisecondi e fino allo scadere del tempo assoluto "abstime".
  * \param sockname -> nome del socket al quale connettersi
- * \param msec -> numero di millisecondi da aspettare prima di ritentare la connessione
+ * \param msec -> tempo in millisecondi da aspettare prima di ritentare la connessione
  * \param abstime -> tempo massimo, scaduto il quale interrompere il tentativo di connessione
  * \retval -> 0 se successo, -1 se errore (setta errno)
  */
@@ -18,95 +16,124 @@ int openConnection(const char* sockname, int msec, const struct timespec abstime
 
 /**
  * Chiude la connessione AF_UNIX associata al socket file sockname. 
- * Ritorna 0 in caso di successo, -1 in caso di fallimento, errno viene settato opportunamente
  * \param sockname -> nome del socket
  * \retval -> 0 se successo, -1 se errore (setta errno)
 */
 int closeConnection(const char* sockname);
 
 /**
- * Richiesta di apertura o di creazione di un file. La semantica della openFile dipende dai flags passati come secondo
- * argomento che possono essere O_CREATE ed O_LOCK. Se viene passato il flag O_CREATE ed il file esiste già
- * memorizzato nel server, oppure il file non esiste ed il flag O_CREATE non è stato specificato, viene ritornato un
- * errore. In caso di successo, il file viene sempre aperto in lettura e scrittura, ed in particolare le scritture possono
- * avvenire solo in append. Se viene passato il flag O_LOCK (eventualmente in OR con O_CREATE) il file viene
- * aperto e/o creato in modalità locked, che vuol dire che l’unico che può leggere o scrivere il file ‘pathname’ è il
- * processo che lo ha aperto. Il flag O_LOCK può essere esplicitamente resettato utilizzando la chiamata unlockFile,
- * descritta di seguito.
- * Ritorna 0 in caso di successo, -1 in caso di fallimento, errno viene settato opportunamente.
- *  flags = 0 -> !O_CREATE && !O_LOCK
- *        = 1 -> O_CREATE && !O_LOCK
- *        = 2 -> !O_CREATE && O_LOCK
- *        = 3 -> O_CREATE && O_LOCK
+ * Richiesta di apertura o di creazione di un file. La semantica dipende dai flags passati come secondo argomento 
+ * (O_CREATE e/o O_LOCK). Se viene passato il flag O_CREATE ed il file esiste gia' memorizzato nel server, 
+ * oppure il file non esiste ed il flag O_CREATE non e' stato specificato, viene ritornato un errore. 
+ * In caso di successo, il file viene aperto in lettura e scrittura, ed in particolare le scritture possono avvenire solo in append. 
+ * Se viene passato il flag O_LOCK (eventualmente in OR con O_CREATE) il file viene aperto e/o creato in modalità locked, 
+ * che vuol dire che l’unico che può leggere o scrivere il file ‘pathname’ e' il processo che lo ha aperto. 
+ * Il flag O_LOCK può essere esplicitamente resettato utilizzando la chiamata unlockFile.
+ * \param pathname -> nome del file da aprire/creare
+ * \param flags -> 0 -> !O_CREATE && !O_LOCK
+ *                 1 -> O_CREATE && !O_LOCK
+ *                 2 -> !O_CREATE && O_LOCK
+ *                 3 -> O_CREATE && O_LOCK
+ * \retval -> 0 se successo, -1 se errore (setta errno)
  */
 int openFile(const char* pathname, int flags);
 
 /**
- * Legge tutto il contenuto del file dal server (se esiste) ritornando un puntatore ad un'area allocata sullo heap nel
- * parametro ‘buf’, mentre ‘size’ conterrà la dimensione del buffer dati (ossia la dimensione in bytes del file letto). 
- * In caso di errore, ‘buf‘e ‘size’ non sono validi. 
- * Ritorna 0 in caso di successo, -1 in caso di fallimento, errno viene settato opportunamente.
+ * Legge tutto il contenuto del file dal server (se esiste) ritornando un puntatore ad un'area allocata sullo heap nel parametro "buf", 
+ * mentre "size" conterrà la dimensione del buffer dati (ossia la dimensione in bytes del file letto). 
+ * In caso di errore, "buf" e "size" non sono validi. 
+ * \param pathfile -> nome del file da leggere
+ * \param buf -> puntatore ad un'area allocata sullo heap dove verra' memorizzato il contenuto del file
+ * \param size -> puntatore alla dimensione del buffer dati (ovvero del file letto)
+ * \retval -> 0 se successo, -1 se errore (setta errno)
  */
 int readFile(const char* pathname, void** buf, size_t* size);
 
 /**
- * Richiede al server la lettura di ‘N’ files qualsiasi da memorizzare nella directory ‘dirname’ lato client. 
- * Se il server ha meno di ‘N’ file disponibili, li invia tutti. 
- * Se N <= 0 la richiesta al server è quella di leggere tutti i file memorizzati al suo interno. 
- * Ritorna un valore maggiore o uguale a 0 in caso di successo (cioè ritorna il n. di file effettivamente letti), 
- * -1 in caso di fallimento, errno viene settato opportunamente.
+ * Richiede al server la lettura di 'N' files qualsiasi da memorizzare nella directory "dirname" lato client. 
+ * Se il server ha meno di 'N' file disponibili, li invia tutti (esclusi quelli che il client non ha il permesso di leggere).
+ * Se N <= 0, la richiesta al server e' quella di leggere tutti i file memorizzati al suo interno. 
+ * \param N -> numero di files da leggere; se <=0, la richiesta e' quella di leggere tutti i file memorizzati nel server
+ * \param dirname -> cartella lato client dove memorizzare i files letti
+ * \retval -> numero >= 0 se successo (numero di file effettivamente letti), -1 se errore (setta errno)
  */
 int readNFiles(int N, const char* dirname);
 
 /**
- * Scrive tutto il file puntato da pathname nel file server. Ritorna successo solo se la precedente operazione,
- * terminata con successo, è stata openFile(pathname, O_CREATE| O_LOCK). Se ‘dirname’ è diverso da NULL, 
- * il file eventualmente spedito dal server perche' espulso dalla cache per far posto al file "pathname" dovra' essere
- * scritto in ‘dirname’; ritorna 0 in caso di successo, -1 in caso di fallimento, errno viene settato opportunamente.
-*/
+ * Scrive tutto il file puntato da "pathname" nel file server. Ritorna successo solo se la precedente operazione, terminata con successo, 
+ * e' stata openFile(pathname, O_CREATE| O_LOCK). Se ‘dirname’ è diverso da NULL, i file eventualmente spediti dal server 
+ * perche' espulsi dalla cache per far posto al file "pathname" verranno scritto in "dirname". 
+ * \param pathname -> nome del file da scrivere sul server
+ * \param dirname -> se != NULL, cartella dove scrivere i file eventualmente espulsi dal server in seguito a capacity misses
+ * \retval -> 0 se successo, -1 se errore (setta errno)
+ */
 int writeFile(const char* pathname, const char* dirname);
 
 /**
- * Richiesta di scrivere in append al file ‘pathname‘ i ‘size‘ bytes contenuti nel buffer ‘buf’. 
- * L’operazione di append nel file è garantita essere atomica dal file server. 
- * Se ‘dirname’ è diverso da NULL, il file eventualmente spedito dal server perchè 
- * espulso dalla cache per far posto ai nuovi dati di ‘pathname’ dovrà essere scritto in ‘dirname’.
- * Ritorna 0 in caso di successo, -1 in caso di fallimento, errno viene settato opportunamente. 
+ * Richiesta di scrivere in append al file "pathname" i "size" bytes contenuti nel buffer "buf". 
+ * L’operazione di append nel file e' garantita essere atomica dal file server. 
+ * Se "dirname" è diverso da NULL, il file eventualmente spedito dal server perche' espulso dalla cache 
+ * per far posto ai nuovi dati di "pathname" dovra' essere scritto in "dirname".
+ * \param pathname -> nome del file su cui scrivere in append
+ * \param buf -> buffer che contiene i dati da scrivere in append sul file
+ * \param size -> dimensione (numero di bytes) del buffer da scrivere in append
+ * \param dirname -> se != NULL, cartella dove scrivere i file espulsi dal server in seguito a capacity misses
+ * \retval -> 0 se successo, -1 se errore (setta errno)
  */
 int appendToFile(const char* pathname, void* buf, size_t size, const char* dirname);
 
 /**
- * In caso di successo setta il flag O_LOCK al file. Se il file era stato aperto/creato con il flag O_LOCK 
- * e la richiesta proviene dallo stesso processo, oppure se il file non ha il flag O_LOCK settato, 
- * l’operazione termina immediatamente con successo, altrimenti l’operazione non viene completata 
+ * Imposta un file nel server in modalita' locked (ovvero setta il flag O_LOCK). Se il file era stato aperto/creato 
+ * con il flag O_LOCK e la richiesta proviene dallo stesso processo, oppure se il file non ha il flag O_LOCK settato, 
+ * l’operazione termina immediatamente con successo; altrimenti, l’operazione non viene completata 
  * fino a quando il flag O_LOCK non viene resettato dal detentore della lock. 
- * L’ordine di acquisizione della lock sul file non è specificato. 
- * Ritorna 0 in caso di successo, -1 in caso di fallimento, errno viene settato opportunamente.
+ * L’ordine di acquisizione della lock sul file non e' specificato. 
+ * \param pathname -> nome del file da impostare in modalita' locked
+ * \retval -> 0 se successo, -1 se errore (setta errno)
  */
 int lockFile(const char* pathname);
 
 /**
- * Resetta il flag O_LOCK sul file ‘pathname’. L’operazione ha successo solo se l’owner della lock 
- * è il processo che  ha richiesto l’operazione, altrimenti l’operazione termina con errore. 
- * Ritorna 0 in caso di successo, -1 in caso di fallimento, errno viene settato opportunamente.
+ * Resetta il flag O_LOCK sul file "pathname". L’operazione ha successo solo se l’owner della lock 
+ * e' il processo che ha richiesto l’operazione, altrimenti termina con errore. 
+ * \param pathname -> nome del file sul quale resettare il flag O_LOCK
+ * \retval -> 0 se successo, -1 se errore (setta errno)
  */
 int unlockFile(const char* pathname);
 
 /**
  * Richiesta di chiusura del file puntato da "pathname". Eventuali operazioni sul file dopo la closeFile falliscono.
- * Ritorna 0 in caso di successo, -1 in caso di fallimento, errno viene settato opportunamente.
  * Quando un file viene chiuso, la mutua esclusione viene rilasciata automaticamente.
+ * \param pathname -> nome del file da chiudere
+ * \retval -> 0 se successo, -1 se errore (setta errno)
  */
 int closeFile(const char *pathname);
 
 /**
- * Rimuove il file cancellandolo dal file storage server. L’operazione fallisce se il file non è in stato locked, 
- * o è in stato locked da parte di un processo client diverso da chi effettua la removeFile.
+ * Rimuove il file cancellandolo dal file storage server. L’operazione fallisce se il file non e' in stato locked, 
+ * o e' in stato locked da parte di un processo client diverso da chi effettua la removeFile.
+ * \param pathname -> nome del file da rimuovere dal server
+ * \retval -> 0 se successo, -1 se errore (setta errno)
  */
 int removeFile(const char* pathname);
 
+/**
+ * A seconda del flag 'rw', imposta la cartella per le scritture dei file eventualmente espulsi dal server in seguito a capacity misses 
+ * provocati da una openFile, oppure imposta la cartella dove scrivere i file letti con la readFile.
+ * \param Dir -> cartella da impostare per la openFile o la readFile
+ * \param rw -> se = 1, imposta la cartella per le scritture dei file espulsi provocate dalla openFile
+ *              se = 0, imposta la cartella dove scrivere i file letti con la readFile
+ */
+int setDirectory(char* Dir, int rw);
 
-// Funzioni ausiliarie
+/**
+ * Abilita o disabilita la stampa delle informazioni interne sulle operazioni effettuate.
+ * \param p -> se = 1, abilita la stampa
+ *             se = 0, disabilita la stampa
+ */
+void printInfo(int p);
+
+// Funzioni ausiliarie; vengono chiamate internamente alle altre funzioni della libreria
 int receiveFile(const char *dirname, void **bufA, size_t *sizeA);
 int receiveNFiles(const char *dirname);
 int lockFile_aux(const char *pathname);
@@ -114,5 +141,3 @@ int addOpenFile(const char *pathname);
 int isOpen(const char *pathname);
 int removeOpenFile(const char *pathname);
 int closeEveryFile();
-void printInfo(int p);
-int setDirectory(char* Dir, int rw);
